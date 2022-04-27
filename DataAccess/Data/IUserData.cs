cf72ba7db0b281aed4d4c6999d6664126132ab03
Tofiq Əmraslanov <tofiq.amraslanov.t@asoiu.edu.az>
2022-04-28 02:11:22 +0400

    DataAccess layer was implemented

---
+++ DataAccess/Data/IUserData.cs
@@ -0,0 +1,12 @@
+﻿using DataAccess.Models;
+
+namespace DataAccess.Data;
+
+public interface IUserData
+{
+    Task<IEnumerable<User>> GetUsers();
+    Task<User?> GetUser(int id);
+    Task InsertUser(User user);
+    Task UpdateUser(User user);
+    Task DeleteUser(int id);
+}
---
+++ DataAccess/Data/UserData.cs
@@ -0,0 +1,32 @@
+﻿using DataAccess.DbAccess;
+using DataAccess.Models;
+
+namespace DataAccess.Data;
+
+public class UserData : IUserData
+{
+    private readonly ISqlDataAccess _sqlDataAccess;
+
+    public UserData(ISqlDataAccess sqlDataAccess) => _sqlDataAccess = sqlDataAccess;
+
+    public async Task<IEnumerable<User>> GetUsers() =>
+        await _sqlDataAccess.LoadData<User, dynamic>("dbo.spUser_GetAll", new { });
+
+    public async Task<User?> GetUser(int id)
+    {
+        var results = await _sqlDataAccess.LoadData<User, dynamic>(
+            "dbo.spUser_GetById",
+            new { Id = id });
+
+        return results.FirstOrDefault();
+    }
+
+    public Task InsertUser(User user) =>
+        _sqlDataAccess.SaveData("dbo.spUser_Insert", new { user.FirstName, user.LastName });
+
+    public Task UpdateUser(User user) =>
+        _sqlDataAccess.SaveData("dbo.spUser_Update", user);
+
+    public Task DeleteUser(int id) =>
+        _sqlDataAccess.SaveData("dbo.spUser_Delete", new { Id = id });
+}

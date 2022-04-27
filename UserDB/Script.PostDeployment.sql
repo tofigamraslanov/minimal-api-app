8c2db0f4cefef2b794a6747eb179b836e95835ab
Tofiq Əmraslanov <tofiq.amraslanov.t@asoiu.edu.az>
2022-04-28 00:50:09 +0400

    Stored Procedures and Tables were created for UserDB

---
+++ UserDB/Script.PostDeployment.sql
@@ -0,0 +1,8 @@
+﻿IF NOT EXISTS (SELECT 1 FROM [dbo].[User])
+BEGIN
+	INSERT INTO [dbo].[User] (FirstName, LastName)
+	VALUES ('Tofig', 'Amraslanov'),
+	('Ulvi', 'Aliyev'),
+	('Ravi', 'Valiyev'),
+	('Raul', 'Vugarov');
+END
---
+++ UserDB/dbo/StoredProcedures/spUser_Delete.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[spUser_Delete]
+	@Id int
+AS
+BEGIN
+	DELETE FROM [dbo].[User]
+	WHERE Id = @Id
+END
